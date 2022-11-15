1bcf51af760acb6bb590fa15cdbcf229f70ddc39
Ayush Tripathi <tripathi.at02@gmail.com>
2022-11-15 12:35:50 +0530

    Notes Apk

--- app/src/main/java/com/example/notes/retrieveSide.java
+++ app/src/main/java/com/example/notes/retrieveSide.java
@@ -21,6 +21,7 @@ public class retrieveSide extends AppCompatActivity {
     TextView notesR;
     Button enter;
     DatabaseReference databaseReference;
+    //checking git
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
